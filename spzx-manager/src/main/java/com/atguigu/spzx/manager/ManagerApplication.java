2ed282ef844453bc0c6d68409248caf305b165c5
lcf <licamelf99@gmail.com>
2024-02-21 18:18:14 +0800

    knife4j的集成

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
@@ -3,7 +3,9 @@ package com.atguigu.spzx.manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.atguigu")
 @SpringBootApplication
 public class ManagerApplication {
     public static void main(String[] args) {
--- spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/IndexController.java
@@ -5,12 +5,15 @@ import com.atguigu.spzx.model.dto.system.LoginDto;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "登录登出模块")
 @RestController
 @RequestMapping("/admin/system/index")
 public class IndexController {
@@ -18,6 +21,7 @@ public class IndexController {
     @Autowired
     SysUserService sysUserService;
 
+    @Operation(summary = "登录接口")
     @PostMapping("login")
     public Result login(@RequestBody LoginDto loginDto){
         LoginVo loginVo = sysUserService.login(loginDto);
